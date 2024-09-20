e2dab1a67bfd08145415ef1c179bd83c661b9040
Shivam_Pandey <pandeyshivam5797@gmail.com>
2024-09-20 12:18:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,58 @@
-import streamlit as st
-import pickle
 import nltk
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-import string
-
-# Download necessary NLTK data
 nltk.download('punkt_tab')
 nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
-
-# Load the model and vectorizer
-@st.cache_resource
-def load_model_and_vectorizer():
-    model = pickle.load(open('model.pkl', 'rb'))
-    tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
-    return model, tfidf
-
-model, tfidf = load_model_and_vectorizer()
 
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
-    
+
     y = []
     for i in text:
         if i.isalnum():
             y.append(i)
-    
+
     text = y[:]
     y.clear()
-    
+
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
-            
+
     text = y[:]
     y.clear()
-    
+
     for i in text:
         y.append(ps.stem(i))
-    
+
     return " ".join(y)
 
+# Load pre-trained vectorizer and model
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+# Streamlit app title
 st.title("Email/SMS Spam Classifier")
 
-input_sms = st.text_area("Enter the message")
+# Input field for user message
+input_sms = st.text_input("Enter the message")
 
+# When 'Predict' button is clicked
 if st.button('Predict'):
-    # 1. preprocess
+
+    # Preprocess the input message
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
+
+    # Convert text to vector
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
+
+    # Predict the label
     result = model.predict(vector_input)[0]
-    # 4. Display
+
+    # Display the result
     if result == 1:
         st.header("Spam")
     else:
